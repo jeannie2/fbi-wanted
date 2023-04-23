d2b9c425403d425f5a8c50eb14976d3cf9658075
jeannie <jnam820@gmail.com>
2023-04-23 12:01:08 +0800

    styles[hash].scss

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -76,7 +76,7 @@ module.exports = {
       filename: 'index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles/styles.[hash].css'
+      filename: 'styles/styles.[hash].scss'
     })
   ]
 }
